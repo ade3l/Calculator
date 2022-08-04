a0aabb9eea387b8686701f81dca7e42c99245cd3
Adeel <adeelasakkeer@gmail.com>
2022-08-04 19:34:44 +0530

    added caclulation function codes

---
+++ script.js
@@ -0,0 +1,17 @@
+let add = (a,b)=> a+b
+let subtract = (a,b)=> a-b
+let mult = (a,b)=> a*b
+let divide = (a,b)=> a/b
+let inverse = a=> -1*a
+let percent = (a,b)=>(a/100)*b
+function AC(){
+    let num1 = 0
+    let num2 = 0
+    let operand = add
+    console.log("hello")
+}
+
+AC()
+
+
+
