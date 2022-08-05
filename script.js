3e856450348f66191894fdaa47ef7405546b2e8c
Adeel <adeelasakkeer@gmail.com>
2022-08-05 12:18:25 +0530

    basic arithmetic fns buttons

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
-let add = (a,b)=> a+b
-let subtract = (a,b)=> a-b
-let mult = (a,b)=> a*b
-let divide = (a,b)=> a/b
-let inverse = a=> -1*a
-let percent = (a,b)=>(a/100)*b
+
+let add = (a,b)=> parseInt(a) + parseInt(b)
+let subtract = (a,b)=> parseInt(a) - parseInt(b)
+let mult = (a,b)=> parseInt(a) * parseInt(b)
+let divide = (a,b)=> parseInt(a) / parseInt(b)
+let inverse = a=> -1*parseInt(a)
+let percent = (a,b)=>(parseInt(a)/100) * parseInt(b)
+
+let num1 = "0"
+let num2 = "0"
+let currentNum = ""
+let operand = add
+let calculation=""
+let result = ""
+
+let resultText = document.querySelector('#result')
+let calculationText = document.querySelector('#calculation')
+let funcButtons = document.querySelectorAll('.function')
+
 function AC(){
-    let num1 = 0
-    let num2 = 0
-    let operand = add
-    console.log("hello")
+    num1 = "0"
+    num2 = "0"
+    currentNum = "";
+    operand = add
+    calculation="";
+    result = "0"
+    updateDisplay()
+}
+
+function updateDisplay(){
+    resultText.innerText = result;
+    if(calculation.length == 0){
+        calculationText.innerHTML = "&ensp;"
+    }
+    else{ calculationText.innerHTML = calculation}
 }
 
 AC()
 
+funcButtons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        if(button.id =="ac") AC();
+        if(calculation!=""){
+            switch(button.id){
+                case 'add': operand = add; break
+                case 'subtract': operand = subtract; break
+                case 'multiply': operand= mult; break
+                case 'divide':operand = divide; break
+            }
+            calculation+=button.innerText
+            result= operand(num1,num2);
+            num1 = result
+            updateDisplay()
+        }
+    })
+})
 
 
